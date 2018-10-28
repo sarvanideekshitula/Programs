e36e9ca77fabf04ce147aa6ea13ca7e1d28dedf4
Vennela.Chava <chava.vinni@gmail.com>
2018-10-28 21:05:16 +0530

    Codeforces Problem

---
+++ CPP programs/Weird Subtraction Process.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long int a, b;
+	cin>>a>>b;
+	bool count = true;
+	while((a != 0 && b != 0) && (a >= (2*b) || b>= (2*a))){
+		if(a>b){
+			a= a-
+		}
+	}
+	cout<<a<<" "<<b<<endl;
+	return 0;
+}
